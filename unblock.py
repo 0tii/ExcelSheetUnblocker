ff1e4d7e0ea6213cc1979933ab738a4dc0688c88
Daniel <49683346+0tii@users.noreply.github.com>
2021-10-19 10:06:59 +0200

    option for file or folder

--- unblock.py
+++ unblock.py
@@ -1,22 +1,31 @@
-import zipfile, re, io
+import zipfile, re, os, sys
 from resources.updatezip import UpdateableZipFile as uzip
 
-excelfile = './test.xlsx'
-sheetfiles = {}
+file = False
+filepath = './test2.xlsx'
+folderpath = './'
 
-strclean = re.compile('<sheetProtection.*?>') #regex exclude sheetProtection tag
+#regex pattern to exclude sheetProtection
+strclean = re.compile('<sheetProtection.*?>') 
 
-# get all sheet paths
-zip = zipfile.ZipFile(excelfile)
-for filename in zip.namelist():
-    if filename.startswith('xl/worksheets/sheet') and filename.endswith('.xml'):
-        ofile = zip.open(filename)
-        sheetfiles[filename] = re.sub(strclean, '', ofile.read().decode('utf-8'))
-zip.close()
+def processfile(excelfile):
+    sheetfiles = {}
 
-# alter files
-with uzip(excelfile) as file:
-    for fname, fcontent in sheetfiles.items():
-        file.writestr(fname, fcontent.encode('utf-8'))
+    # get all target sheet paths
+    with zipfile.ZipFile(excelfile) as zip:
+        for filename in zip.namelist():
+            if filename.startswith('xl/worksheets/sheet') and filename.endswith('.xml'):
+                ofile = zip.open(filename)
+                sheetfiles[filename] = re.sub(strclean, '', ofile.read().decode('utf-8'))
 
+    # alter files
+    with uzip(excelfile) as file:
+        for fname, fcontent in sheetfiles.items():
+            file.writestr(fname, fcontent.encode('utf-8'))
 
+if file:
+    processfile(filepath)
+else:
+    for item in os.listdir(folderpath):
+        if item.endswith('.xlsx'):
+            processfile(item)
