1bf378da552867fff5dd6f711ad680cad117dfb1
Daniel <49683346+0tii@users.noreply.github.com>
2021-10-19 09:27:03 +0200

    working prototype

--- unblock.py
+++ unblock.py
@@ -1,12 +1,22 @@
-import zipfile, os, tempfile, fnmatch
+import zipfile, re, io
 from resources.updatezip import UpdateableZipFile as uzip
 
-fpaths = []
+excelfile = './test.xlsx'
+sheetfiles = {}
+
+strclean = re.compile('<sheetProtection.*?>') #regex exclude sheetProtection tag
 
 # get all sheet paths
-zip = zipfile.ZipFile('./test.xlsx')
+zip = zipfile.ZipFile(excelfile)
 for filename in zip.namelist():
     if filename.startswith('xl/worksheets/sheet') and filename.endswith('.xml'):
-        fpaths.append(filename)
+        ofile = zip.open(filename)
+        sheetfiles[filename] = re.sub(strclean, '', ofile.read().decode('utf-8'))
+zip.close()
+
+# alter files
+with uzip(excelfile) as file:
+    for fname, fcontent in sheetfiles.items():
+        file.writestr(fname, fcontent.encode('utf-8'))
 
 
